b86e79801c1e27c7d12a02116a980346dc61a060
Inaldo Ribeiro <inaldoribeiro.eireli@poupex.com.br>
2024-08-22 20:23:43 -0300

    Adicionando projeto.

---
+++ sorteio-online/src/main/java/br/com/sorterio/online/SorteioOnlineApplication.java
@@ -0,0 +1,13 @@
+package br.com.sorterio.online;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SorteioOnlineApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SorteioOnlineApplication.class, args);
+	}
+
+}
---
+++ sorteio-online/src/test/java/br/com/sorterio/online/SorteioOnlineApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.sorterio.online;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SorteioOnlineApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
